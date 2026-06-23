d9074c1d2940e86efb06790e4f43c842234de04a
Wouter Deconinck <wdconinc@gmail.com>
2026-06-23 15:51:48 -0500

    fix: spack  cherry-pick: gaudi: workaround test-dependency bug with a when

--- spack-packages.sh
+++ spack-packages.sh
@@ -14,6 +14,7 @@ read -r -d '' SPACKPACKAGES_CHERRYPICKS <<- \
 --- || true
 a115a811bdfce4db5298a9ba9b7903ccfb0de101
 20444b8e9382e659360a1446688d10a8c2d2ad31
+f5742718da7bd1d078ddc8423011a82ef2e3c759
 deb4f17d93dbe012403614245334f7c73fcc086f
 0d64b2bab72a99441f042b663e4bc993ec0db45d
 a77a7ed0d2630466cac71165026387b1381b058a
@@ -28,6 +29,7 @@ read -r -d '' SPACKPACKAGES_CHERRYPICKS_FILES <<- \
 ## [hash]: [description]
 ## a115a811bdfce4db5298a9ba9b7903ccfb0de101: github-copilot: new package
 ## 20444b8e9382e659360a1446688d10a8c2d2ad31: github-copilot: add v1.0.8
+## f5742718da7bd1d078ddc8423011a82ef2e3c759: gaudi: workaround test-dependency bug with a when
 ## deb4f17d93dbe012403614245334f7c73fcc086f: fix: add latest osg-ca-cert
 ## 0d64b2bab72a99441f042b663e4bc993ec0db45d: osg-ca-certs: depends on gmake and perl, type build
 ## a77a7ed0d2630466cac71165026387b1381b058a: py-pynacl: depends on gmake, type build
